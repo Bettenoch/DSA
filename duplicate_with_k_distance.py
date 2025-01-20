d42bdd85fc9d578d2118f721a7bb5fbab6a640d9
Bettenoch <bettenoch254@gmail.com>
2025-01-20 11:04:14 +0300

    test sets

--- duplicate_with_k_distance.py
+++ duplicate_with_k_distance.py
@@ -1,15 +1,15 @@
-def duplicateKDistance(arr, k):
-    
-    test = arr[:k+1]
-    
+def duplicate_with_k_distance(arr, k):
+    test = arr[:k + 1]
+
     present = set()
-    
+
     for num in test:
         if num in present:
             return True
         present.add(num)
     return False
 
-arr = [1, 4, 6, 4, 8, 11, 1]
+
+arr2 = [1, 4, 6, 4, 8, 11, 1]
 
 print(duplicateKDistance(arr, 3))
