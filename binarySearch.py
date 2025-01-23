6082cb45393f25621174ac00fe719378dfb633a2
Bettenoch <bettenoch254@gmail.com>
2025-01-23 08:30:49 +0300

    Modify structure

--- binarySearch.py
+++ binarySearch.py
@@ -1,17 +1,19 @@
 def swap(arr, i, k):
     arr[i], arr[k] = arr[k], arr[i]
 
+
 def Partition(arr, low, high):
     pivot = arr[high]
     i = low - 1
-    
+
     for k in range(low, high):
         if arr[k] < pivot:
             i += 1
             swap(arr, i, k)
-            
+
     swap(arr, i + 1, high)
     return i + 1
+
 
 def QuickSort(arr, low, high):
     if low < high:
@@ -19,9 +21,11 @@ def QuickSort(arr, low, high):
         QuickSort(arr, low, mid - 1)
         QuickSort(arr, mid + 1, high)
 
+
 def quicksort(arr):
     QuickSort(arr, 0, len(arr) - 1)
     return arr  # Return the sorted array
+
 
 # Example usage
 arr = [10, 7, 8, 9, 1, 5]
