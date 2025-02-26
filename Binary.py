d7b649fa5aeba34f39010d192ec466bd01980a7b
Bettenoch <bettenoch254@gmail.com>
2025-02-26 08:58:41 +0300

    Implement binary search

--- Binary.py
+++ Binary.py
@@ -1,23 +1,25 @@
-def BinarySearch(arr, item):
+from typing import Any
+
+
+def binary_search(arr, item: int) -> Any:
     low = 0
-    high = len(arr)-1
-    
+    high = len(arr) - 1
+
     while low <= high:
         mid = low + (high + low) // 2
-        
+
         guess = arr[mid]
-        
+
         if guess == item:
             return mid
         if guess < item:
             low = mid + 1
-            
+
         else:
             high = mid - 1
-        return (f"{item} not found")
-    
-    
+        return f"{item}not found"
+
+
 scores = [1, 5, 7, 14, 88, 13, 2, 34]
 
-print(BinarySearch(scores, 13))
-            
+print(binary_search(scores, 13))
