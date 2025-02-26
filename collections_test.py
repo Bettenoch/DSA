16098f12446efd7e06e35013e747e783ce124203
Bettenoch <bettenoch254@gmail.com>
2025-02-26 15:20:11 +0300

    Implement binary search

--- collections_test.py
+++ collections_test.py
@@ -1,4 +1,5 @@
 from collections import deque
+import heapq
 
 q = deque(maxlen=3)
 
@@ -6,11 +7,28 @@ q.append(1)
 q.append(3)
 q.append(8)
 
-print(q)
+# print(q)
 
 q.extendleft([9])
-print(q)
+# print(q)
 
 q.popleft()
 
-print(q)
+# print(q)
+
+nums = [2, 5, -8, 89, 19, 22]
+
+change = list(nums)
+print("First One", change)
+heapq.heapify(change)
+heapq.heappush(-22, change)
+
+print(change)
+
+
+smallest = heapq.nlargest(3, nums)
+# print(smallest)
+
+
+
+
